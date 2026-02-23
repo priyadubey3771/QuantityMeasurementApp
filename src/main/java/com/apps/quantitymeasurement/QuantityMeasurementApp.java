d64713affb806a71023963ea3c3d3ad0e62073a7
priyadubey3771 <priyadubey@3771>
2026-02-23 11:51:35 +0530

    [Priya]: added QuantityMeasurementApp

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,13 +2,24 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+    public static boolean checkFeetEquality(double value1, double value2) {
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+        return f1.equals(f2);
+    }
+
+    public static boolean checkInchEquality(double value1, double value2) {
+        Inches i1 = new Inches(value1);
+        Inches i2 = new Inches(value2);
+        return i1.equals(i2);
+    }
+
     public static void main(String[] args) {
 
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
+        System.out.println("1.0 inch and 1.0 inch: " +
+                checkInchEquality(1.0, 1.0));
 
-        boolean result = feet1.equals(feet2);
-
-        System.out.println("Are the two measurements equal? " + result);
+        System.out.println("1.0 ft and 1.0 ft: " +
+                checkFeetEquality(1.0, 1.0));
     }
 }

0500af57f6f861813145bd2d52416205755f309d
priyadubey3771 <priyadubey@3771>
2026-02-23 21:34:32 +0530

    [Priya]: added IMeasurable

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,16 +1,6 @@
 package com.apps.quantitymeasurement;
 
-public interface IMeasurable {
-
+public interface IMeasurable 
+{
     double getConversionFactor();
-
-    default double convertToBaseUnit(double value) {
-        return value * getConversionFactor();
-    }
-
-    default double convertFromBaseUnit(double baseValue) {
-        return baseValue / getConversionFactor();
-    }
-
-    String getUnitName();
 }

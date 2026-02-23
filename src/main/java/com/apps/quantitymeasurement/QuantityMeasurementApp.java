dea51395d737f79c60fe80642e2719f4ede06856
priyadubey3771 <priyadubey@3771>
2026-02-23 15:37:59 +0530

    [Priya]: added QuantityMeasurementApp

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,21 +1,17 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
         QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
         QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        System.out.println(QuantityLength.add(q1, q2, LengthUnit.FEET));
-        System.out.println(QuantityLength.add(q1, q2, LengthUnit.INCHES));
-        System.out.println(QuantityLength.add(q1, q2, LengthUnit.YARDS));
+        System.out.println(q1.convertTo(LengthUnit.INCHES));
+        System.out.println(q1.add(q2, LengthUnit.FEET));
+        System.out.println(new QuantityLength(36.0, LengthUnit.INCHES).equals(new QuantityLength(1.0, LengthUnit.YARDS)));
 
-        QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
-        QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
+        System.out.println(new QuantityLength(1.0, LengthUnit.YARDS).add(new QuantityLength(3.0, LengthUnit.FEET), LengthUnit.YARDS));
 
-        System.out.println(QuantityLength.add(yard, feet, LengthUnit.YARDS));
-        System.out.println(QuantityLength.add(new QuantityLength(36.0, LengthUnit.INCHES),
-                new QuantityLength(1.0, LengthUnit.YARDS), LengthUnit.FEET));
+        System.out.println(new QuantityLength(2.54, LengthUnit.CENTIMETERS).convertTo(LengthUnit.INCHES));
     }
 }

5ddb588f17d55aac1f6a13834f75db0b96e12bcf
priyadubey3771 <priyadubey@3771>
2026-02-23 21:38:46 +0530

    [Priya]: added WeightUnit

--- src/main/java/com/apps/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -2,22 +2,18 @@ package com.apps.quantitymeasurement;
 
 public enum WeightUnit implements IMeasurable {
 
-    KILOGRAM(1000.0),
-    GRAM(1.0);
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
 
     private final double conversionFactor;
 
-    WeightUnit(double conversionFactor) {
-        this.conversionFactor = conversionFactor;
+    WeightUnit(double factor) {
+        this.conversionFactor = factor;
     }
 
     @Override
     public double getConversionFactor() {
         return conversionFactor;
-    }
-
-    @Override
-    public String getUnitName() {
-        return this.name();
     }
 }

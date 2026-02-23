8b1638d80b5a9389b06b6530ccc61d0f24babb98
priyadubey3771 <priyadubey@3771>
2026-02-23 17:16:54 +0530

    [Priya]: added LengthUnit

---
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -0,0 +1,25 @@
+package com.apps.quantitymeasurement;
+
+public enum LengthUnit implements IMeasurable {
+
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return this.name();
+    }
+}

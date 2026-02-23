e28fa45fc2fb35525d4db8e2705d9d57b55753eb
priyadubey3771 <priyadubey@3771>
2026-02-23 15:10:45 +0530

    [Priya]: added QuantityMeasurementApp

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -7,13 +7,15 @@ public class QuantityMeasurementApp {
         QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
         QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        QuantityLength result = q1.add(q2);
-
-        System.out.println(result); // Expected: Quantity(2.0, FEET)
+        System.out.println(QuantityLength.add(q1, q2, LengthUnit.FEET));
+        System.out.println(QuantityLength.add(q1, q2, LengthUnit.INCHES));
+        System.out.println(QuantityLength.add(q1, q2, LengthUnit.YARDS));
 
         QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
         QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
 
-        System.out.println(yard.add(feet)); // Expected: Quantity(2.0, YARDS)
+        System.out.println(QuantityLength.add(yard, feet, LengthUnit.YARDS));
+        System.out.println(QuantityLength.add(new QuantityLength(36.0, LengthUnit.INCHES),
+                new QuantityLength(1.0, LengthUnit.YARDS), LengthUnit.FEET));
     }
 }

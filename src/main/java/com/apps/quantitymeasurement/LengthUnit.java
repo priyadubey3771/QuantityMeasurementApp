8e6d10a956e0cfdbcfa191064ab601cfaf7ff9c8
priyadubey3771 <priyadubey@3771>
2026-02-23 13:22:28 +0530

    [Priya]: added LengthUnit

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -2,18 +2,18 @@ package com.apps.quantitymeasurement;
 
 public enum LengthUnit {
 
- FEET(1.0),
- INCH(1.0 / 12.0),          // 1 inch = 1/12 feet
- YARDS(3.0),               // 1 yard = 3 feet
- CENTIMETERS(0.0328084);   // 1 cm = 0.0328084 feet (derived from 0.393701 inches)
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(0.0328084); // 1 cm = 0.0328084 feet
 
- private final double conversionFactorToFeet;
+    private final double conversionFactorToFeet;
 
- LengthUnit(double conversionFactorToFeet) {
-     this.conversionFactorToFeet = conversionFactorToFeet;
- }
+    LengthUnit(double conversionFactorToFeet) {
+        this.conversionFactorToFeet = conversionFactorToFeet;
+    }
 
- public double toFeet(double value) {
-     return value * conversionFactorToFeet;
- }
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
+    }
 }

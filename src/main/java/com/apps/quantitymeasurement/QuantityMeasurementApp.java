958cb0c89e09e7f4928c89704cc5e52844020d5b
priyadubey3771 <priyadubey@3771>
2026-02-23 12:54:04 +0530

    [Priya]: added QuantityMeasurementApp

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,14 +3,10 @@ package com.apps.quantitymeasurement;
 public class QuantityMeasurementApp {
  public static void main(String[] args) {
 
-	        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-	        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+	 System.out.println(new QuantityLength(1.0, LengthUnit.YARDS).equals(new QuantityLength(3.0, LengthUnit.FEET)));
 
-	        System.out.println("Quantity(1.0, FEET) and Quantity(12.0, INCH): " + q1.equals(q2));
+	         System.out.println(new QuantityLength(1.0, LengthUnit.YARDS).equals(new QuantityLength(36.0, LengthUnit.INCH)));
 
-	        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
-	        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
-
-	        System.out.println("Quantity(1.0, INCH) and Quantity(1.0, INCH): "+ q3.equals(q4));
-	    }
-	}
+	         System.out.println(new QuantityLength(1.0, LengthUnit.CENTIMETERS).equals(new QuantityLength(0.393701, LengthUnit.INCH)));
+	     }
+	 }
